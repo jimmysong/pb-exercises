51059c1f6f5052beb1147e38c2c709db53151f91
Jimmy Song <jaejoon@gmail.com>
2017-10-02 22:44:44 -0700

    session2

--- ecc.py
+++ ecc.py
@@ -1,4 +1,9 @@
+from binascii import hexlify, unhexlify
+from io import BytesIO
+from random import randint
 from unittest import TestCase
+
+from helper import double_sha256, encode_base58, hash160
 
 
 class FieldElement:
@@ -57,6 +62,10 @@ class FieldElement:
         # use: self.__class__(num, prime)
         return self.__class__(num, prime)
 
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+        return self.__class__(num=num, prime=self.prime)
+
     def __pow__(self, n):
         # remember fermat's little theorem:
         # self.num**(p-1) % p == 1
@@ -104,7 +113,12 @@ class FieldElementTest(TestCase):
         a = FieldElement(24, 31)
         b = FieldElement(19, 31)
         self.assertEqual(a*b, FieldElement(22, 31))
-        
+
+    def test_rmul(self):
+        a = FieldElement(24, 31)
+        b = 2
+        self.assertEqual(b*a, a+a)
+
     def test_pow(self):
         a = FieldElement(17, 31)
         self.assertEqual(a**3, FieldElement(15, 31))
@@ -199,6 +213,26 @@ class Point:
         # self.__class__(x, y, a, b)
             return self.__class__(x, y, self.a, self.b)
 
+    def __rmul__(self, coefficient):
+        # rmul calculates coefficient * self
+        # implement the naive way:
+        # start product from 0 (point at infinity)
+        # use: self.__class__(None, None, a, b)
+        product = self.__class__(None, None, self.a, self.b)
+        # loop coefficient times
+        # use: for _ in range(coefficient):
+        for _ in range(coefficient):
+            # keep adding self over and over
+            product += self
+        # return the product
+        return product
+        # Extra Credit:
+        # a more advanced technique uses point doubling
+        # find the binary representation of coefficient
+        # keep doubling the point and if the bit is there for coefficient
+        # add the current.
+        # remember to return an instance of the class
+
 
 class PointTest(TestCase):
 
@@ -225,3 +259,343 @@ class PointTest(TestCase):
     def test_add2(self):
         a = Point(x=-1, y=1, a=5, b=7)
         self.assertEqual(a+a, Point(x=18, y=-77, a=5, b=7))
+
+
+class ECCTest(TestCase):
+
+    def test_on_curve(self):
+        # tests the following points whether they are on the curve or not
+        # on curve y^2=x^3-7 over F_223:
+        # (192,105) (17,56) (200,119) (1,193) (42,99)
+        # the ones that aren't should raise a RuntimeError
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+        
+        valid_points = ((192,105), (17,56), (1,193))
+        invalid_points = ((200,119), (42,99))
+        
+        # iterate over valid points
+            # Initialize points this way:
+            # x = FieldElement(192, prime)
+            # y = FieldElement(105, prime)
+            # Point(x, y, a, b)
+            # Creating the point should not result in an error
+
+        # iterate over invalid points
+            # Initialize points this way:
+            # x = FieldElement(192, prime)
+            # y = FieldElement(105, prime)
+            # Point(x, y, a, b)
+            # check that creating the point results in a RuntimeError
+            # with self.assertRaises(RuntimeError):
+            #     Point(x, y, a, b)
+        raise NotImplementedError
+
+    def test_add1(self):
+        # tests the following additions on curve y^2=x^3-7 over F_223:
+        # (192,105) + (17,56)
+        # (47,71) + (117,141)
+        # (143,98) + (76,66)
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+
+        additions = (
+            # (x1, y1, x2, y2, x3, y3)         
+            (192, 105, 17, 56, 170, 142),
+            (47, 71, 117, 141, 60, 139),
+            (143, 98, 76, 66, 47, 71),
+        )
+        # iterate over the additions
+            # Initialize points this way:
+            # x = FieldElement(192, prime)
+            # y = FieldElement(105, prime)
+            # p1 = Point(x, y, a, b)
+            # check that p1 + p2 == p3
+        raise NotImplementedError
+
+    def test_rmul(self):
+        # tests the following scalar multiplications
+        # 2*(192,105)
+        # 2*(143,98)
+        # 2*(47,71)
+        # 4*(47,71)
+        # 8*(47,71)
+        # 21*(47,71)
+        prime = 223
+        a = FieldElement(0, prime)
+        b = FieldElement(7, prime)
+
+        multiplications = (
+            # (coefficient, x1, y1, x2, y2)
+            (2, 192, 105, 49, 71),
+            (2, 143, 98, 64, 168),
+            (2, 47, 71, 36, 111),
+            (4, 47, 71, 194, 51),
+            (8, 47, 71, 116, 55),
+            (21, 47, 71, None, None),
+        )
+
+        # iterate over the multiplications
+            # Initialize points this way:
+            # x = FieldElement(192, prime)
+            # y = FieldElement(105, prime)
+            # p = Point(x, y, a, b)
+            # initialize the second point based on whether it's the point at infinity
+            # check that the product is equal to the expected point
+        raise NotImplementedError
+
+A = 0
+B = 7
+P = 2**256 - 2**32 - 977
+N = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
+
+
+class S256Field(FieldElement):
+
+    def __init__(self, num, prime=None):
+        super().__init__(num=num, prime=P)
+
+    def hex(self):
+        return '{:x}'.format(self.num).zfill(64)
+
+    def __repr__(self):
+        return self.hex()
+
+
+class S256Point(Point):
+    bits = 256
+
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if x is None:
+            super().__init__(x=None, y=None, a=a, b=b)
+        elif type(x) == int:
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
+        else:
+            super().__init__(x=x, y=y, a=a, b=b)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        else:
+            return 'Point({},{})'.format(self.x, self.y)
+
+    def __rmul__(self, coefficient):
+        # current will undergo binary expansion
+        current = self
+        # result is what we return, starts at 0
+        result = S256Point(None, None)
+        # we double 256 times and add where there is a 1 in the binary
+        # representation of coefficient
+        for i in range(self.bits):
+            if coefficient & 1:
+                result += current
+            current += current
+            # we shift the coefficient to the right
+            coefficient >>= 1
+        return result
+
+    def sec(self, compressed=True):
+        # returns the binary version of the sec format, NOT hex
+        # if compressed, starts with b'\x02' if self.y.num is even, b'\x03' if self.y is odd
+        # then self.x.num
+        # remember, you have to convert self.x.num/self.y.num to binary (some_integer.to_bytes(32, 'big'))
+        # if non-compressed, starts with b'\x04' followod by self.x and then self.y
+        raise NotImplementedError
+
+    def address(self, compressed=True, testnet=False):
+        '''Returns the address string'''
+        # get the sec
+        # hash160 the sec
+        # raw is hash 160 prepended w/ b'\x00' for mainnet, b'\x6f' for testnet
+        # checksum is first 4 bytes of double_sha256 of raw
+        # encode_base58 the raw + checksum
+        # return as a string, you can use .decode('ascii') to do this.
+        raise NotImplementedError
+
+    def verify(self, z, sig):
+        # remember sig.r and sig.s are the main things we're checking
+        # remember 1/s = pow(s, N-2, N)
+        # u = z / s
+        # v = r / s
+        # u*G + v*P should have as the x coordinate, r
+        raise NotImplementedError
+
+
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+
+
+class S256Test(TestCase):
+
+    def test_order(self):
+        point = N*G
+        self.assertIsNone(point.x)
+
+    def test_pubpoint(self):
+        # write a test that tests the public point for the following
+        points = (
+            # secret, x, y
+            (7, 0x5cbdf0646e5db4eaa398f365f2ea7a0e3d419b7e0330e39ce92bddedcac4f9bc, 0x6aebca40ba255960a3178d6d861a54dba813d0b813fde7b5a5082628087264da),
+            (1485, 0xc982196a7466fbbbb0e27a940b6af926c1a74d5ad07128c82824a11b5398afda, 0x7a91f9eae64438afb9ce6448a1c133db2d8fb9254e4546b6f001637d50901f55),
+            (2**128, 0x8f68b9d2f63b5f339239c1ad981f162ee88c5678723ea3351b7b444c9ec4c0da, 0x662a9f2dba063986de1d90c2b6be215dbbea2cfe95510bfdf23cbf79501fff82),
+            (2**240+2**31, 0x9577ff57c8234558f293df502ca4f09cbc65a6572c842b39b366f21717945116, 0x10b49c67fa9365ad7b90dab070be339a1daf9052373ec30ffae4f72d5e66d053),
+        )
+
+        # iterate over points
+            # initialize the secp256k1 point (S256Point)
+            # check that the secret*G is the same as the point
+        raise NotImplementedError
+
+    def test_sec(self):
+        coefficient = 999**3
+        uncompressed = '049d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d56fa15cc7f3d38cda98dee2419f415b7513dde1301f8643cd9245aea7f3f911f9'
+        compressed = '039d5ca49670cbe4c3bfa84c96a8c87df086c6ea6a24ba6b809c9de234496808d5'
+        point = coefficient*G
+        self.assertEqual(point.sec(compressed=False), unhexlify(uncompressed))
+        self.assertEqual(point.sec(compressed=True), unhexlify(compressed))
+        coefficient = 123
+        uncompressed = '04a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5204b5d6f84822c307e4b4a7140737aec23fc63b65b35f86a10026dbd2d864e6b'
+        compressed = '03a598a8030da6d86c6bc7f2f5144ea549d28211ea58faa70ebf4c1e665c1fe9b5'
+        point = coefficient*G
+        self.assertEqual(point.sec(compressed=False), unhexlify(uncompressed))
+        self.assertEqual(point.sec(compressed=True), unhexlify(compressed))
+        coefficient = 42424242
+        uncompressed = '04aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e21ec53f40efac47ac1c5211b2123527e0e9b57ede790c4da1e72c91fb7da54a3'
+        compressed = '03aee2e7d843f7430097859e2bc603abcc3274ff8169c1a469fee0f20614066f8e'
+        point = coefficient*G
+        self.assertEqual(point.sec(compressed=False), unhexlify(uncompressed))
+        self.assertEqual(point.sec(compressed=True), unhexlify(compressed))
+
+    def test_address(self):
+        secret = 888**3
+        mainnet_address = '148dY81A9BmdpMhvYEVznrM45kWN32vSCN'
+        testnet_address = 'mieaqB68xDCtbUBYFoUNcmZNwk74xcBfTP'
+        point = secret*G
+        self.assertEqual(
+            point.address(compressed=True, testnet=False), mainnet_address)
+        self.assertEqual(
+            point.address(compressed=True, testnet=True), testnet_address)
+        secret = 321
+        mainnet_address = '1S6g2xBJSED7Qr9CYZib5f4PYVhHZiVfj'
+        testnet_address = 'mfx3y63A7TfTtXKkv7Y6QzsPFY6QCBCXiP'
+        point = secret*G
+        self.assertEqual(
+            point.address(compressed=False, testnet=False), mainnet_address)
+        self.assertEqual(
+            point.address(compressed=False, testnet=True), testnet_address)
+        secret = 4242424242
+        mainnet_address = '1226JSptcStqn4Yq9aAmNXdwdc2ixuH9nb'
+        testnet_address = 'mgY3bVusRUL6ZB2Ss999CSrGVbdRwVpM8s'
+        point = secret*G
+        self.assertEqual(
+            point.address(compressed=False, testnet=False), mainnet_address)
+        self.assertEqual(
+            point.address(compressed=False, testnet=True), testnet_address)
+
+    def test_verify(self):
+        point = S256Point(
+            0x887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c,
+            0x61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34)
+        z = 0xec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60
+        r = 0xac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395
+        s = 0x68342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4
+        self.assertTrue(point.verify(z, Signature(r, s)))
+        z = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+        r = 0xeff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c
+        s = 0xc7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6
+        self.assertTrue(point.verify(z, Signature(r, s)))
+
+
+
+class Signature:
+
+    def __init__(self, r, s):
+        self.r = r
+        self.s = s
+
+    def __repr__(self):
+        return 'Signature({:x},{:x})'.format(self.r, self.s)
+
+    def der(self):
+        rbin = self.r.to_bytes(32, byteorder='big')
+        # if rbin has a high bit, add a 00
+        if rbin[0] > 128:
+            rbin = b'\x00' + rbin
+        result = bytes([2, len(rbin)]) + rbin
+        sbin = self.s.to_bytes(32, byteorder='big')
+        # if sbin has a high bit, add a 00
+        if sbin[0] > 128:
+            sbin = b'\x00' + sbin
+        result += bytes([2, len(sbin)]) + sbin
+        return bytes([0x30, len(result)]) + result
+
+    @classmethod
+    def parse(cls, signature_bin):
+        s = BytesIO(signature_bin)
+        compound = s.read(1)[0]
+        if compound != 0x30:
+            raise RuntimeError("Bad Signature")
+        length = s.read(1)[0]
+        if length + 2 != len(signature_bin):
+            raise RuntimeError("Bad Signature Length")
+        marker = s.read(1)[0]
+        if marker != 0x02:
+            raise RuntimeError("Bad Signature")
+        rlength = s.read(1)[0]
+        r = int(hexlify(s.read(rlength)), 16)
+        marker = s.read(1)[0]
+        if marker != 0x02:
+            raise RuntimeError("Bad Signature")
+        slength = s.read(1)[0]
+        s = int(hexlify(s.read(slength)), 16)
+        if len(signature_bin) != 6 + rlength + slength:
+            raise RuntimeError("Signature too long")
+        return cls(r, s)
+
+
+class SignatureTest(TestCase):
+
+    def test_der(self):
+        testcases = (
+            (1, 2),
+            (randint(0, 2**256), randint(0, 2**255)),
+            (randint(0, 2**256), randint(0, 2**255)),
+        )
+        for r, s in testcases:
+            sig = Signature(r, s)
+            der = sig.der()
+            sig2 = Signature.parse(der)
+            self.assertEqual(sig2.r, r)
+            self.assertEqual(sig2.s, s)
+
+
+class PrivateKey:
+
+    def __init__(self, secret):
+        self.secret = secret
+        self.point = secret*G
+
+    def hex(self):
+        return '{:x}'.format(self.secret).zfill(64)
+
+    def sign(self, z):
+        # we need a random number k: randint(0, 2**256)
+        # r is the x coordinate of the resulting point k*G
+        # remember 1/k = pow(k, N-2, N)
+        # s = (z+r*secret) / k
+        # return an instance of Signature:
+        # Signature(r, s)
+        raise NotImplementedError
+
+
+class PrivateKeyTest(TestCase):
+
+    def test_sign(self):
+        pk = PrivateKey(randint(0, 2**256))
+        z = randint(0, 2**256)
+        sig = pk.sign(z)
+        self.assertTrue(pk.point.verify(z, sig))
--- helper.py
+++ helper.py
@@ -1,4 +1,7 @@
+from binascii import hexlify
 from unittest import TestCase, TestSuite, TextTestRunner
+
+import hashlib
 
 
 def run_test(test):
@@ -15,6 +18,36 @@ def bytes_to_str(b, encoding='ascii'):
 def str_to_bytes(s, encoding='ascii'):
     '''Returns a bytes version of the string'''
     return s.encode(encoding)
+
+
+BASE58_ALPHABET = b'123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+
+def hash160(s):
+    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()
+
+
+def double_sha256(s):
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+
+def encode_base58(s):
+    # determine how many 0 bytes (b'\x00') s starts with
+    count = 0
+    for c in s:
+        if c == 0:
+            count += 1
+        else:
+            break
+    prefix = b'1' * count
+    # convert from binary to hex, then hex to integer
+    num = int(hexlify(s), 16)
+    result = bytearray()
+    while num > 0:
+        num, mod = divmod(num, 58)
+        result.insert(0, BASE58_ALPHABET[mod])
+
+    return prefix + bytes(result)
 
 
 class HelperTest(TestCase):
