6f40e159ce63f647dd79f1cf39479ddff4fbec7a
Jimmy Song <jaejoon@gmail.com>
2017-10-20 20:39:48 -0700

    session 1

---
+++ ecc.py
@@ -0,0 +1,189 @@
+from unittest import TestCase
+
+
+class FieldElement:
+
+    def __init__(self, num, prime):
+        self.num = num
+        self.prime = prime
+        if self.num >= self.prime or self.num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                self.num, self.prime-1)
+            raise RuntimeError(error)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        if other is None:
+            return True
+        return self.num != other.num or self.prime != other.prime
+
+    def __repr__(self):
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+    def __add__(self, other):
+        # self.num and other.num are the actual values
+        # self.prime is what you'll need to mod against
+        # You need to return an element of the same class
+        # use: self.__class__(num, prime)
+        raise NotImplementedError
+
+    def __sub__(self, other):
+        # self.num and other.num are the actual values
+        # self.prime is what you'll need to mod against
+        # You need to return an element of the same class
+        # use: self.__class__(num, prime)
+        raise NotImplementedError
+
+    def __mul__(self, other):
+        # self.num and other.num are the actual values
+        # self.prime is what you'll need to mod against
+        # You need to return an element of the same class
+        # use: self.__class__(num, prime)
+        raise NotImplementedError
+
+    def __pow__(self, n):
+        # remember fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # you might want to use % operator on n
+        raise NotImplementedError
+
+    def __truediv__(self, other):
+        # self.num and other.num are the actual values
+        # self.prime is what you'll need to mod against
+        # use fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        # You need to return an element of the same class
+        # use: self.__class__(num, prime)
+        raise NotImplementedError
+
+
+
+class FieldElementTest(TestCase):
+
+    def test_add(self):
+        a = FieldElement(2, 31)
+        b = FieldElement(15, 31)
+        self.assertEqual(a+b, FieldElement(17, 31))
+        a = FieldElement(17, 31)
+        b = FieldElement(21, 31)
+        self.assertEqual(a+b, FieldElement(7, 31))
+
+    def test_sub(self):
+        a = FieldElement(29, 31)
+        b = FieldElement(4, 31)
+        self.assertEqual(a-b, FieldElement(25, 31))
+        a = FieldElement(15, 31)
+        b = FieldElement(30, 31)
+        self.assertEqual(a-b, FieldElement(16, 31))
+
+    def test_mul(self):
+        a = FieldElement(24, 31)
+        b = FieldElement(19, 31)
+        self.assertEqual(a*b, FieldElement(22, 31))
+        
+    def test_pow(self):
+        a = FieldElement(17, 31)
+        self.assertEqual(a**3, FieldElement(15, 31))
+        a = FieldElement(5, 31)
+        b = FieldElement(18, 31)
+        self.assertEqual(a**5 * b, FieldElement(16, 31))
+
+    def test_div(self):
+        a = FieldElement(3, 31)
+        b = FieldElement(24, 31)
+        self.assertEqual(a/b, FieldElement(4, 31))
+        a = FieldElement(17, 31)
+        self.assertEqual(a**-3, FieldElement(29, 31))
+        a = FieldElement(4, 31)
+        b = FieldElement(11, 31)
+        self.assertEqual(a**-4*b, FieldElement(13, 31))
+
+
+
+class Point:
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        # x being None and y being None represents the point at infinity
+        # Check for that here since the equation below won't make sense
+        # with None values for both.
+        # make sure that the elliptic curve equation is satisfied
+        # y**2 == x**3 + a*x + b
+        # if not, throw a RuntimeError
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y \
+            or self.a != other.a or self.b != other.b
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        else:
+            return 'Point({},{})'.format(self.x, self.y)
+
+    def __add__(self, other):
+        # Case 0.0: self is the point at infinity, return other
+        # Case 0.1: other is the point at infinity, return self
+
+        # Case 1: self.x != other.x
+        # Formula (x3,y3)==(x1,y1)+(x2,y2)
+        # s=(y2-y1)/(x2-x1)
+        # x3=s**2-x1-x2
+        # y3=s*(x1-x3)-y1
+        # Remember to return an instance of this class:
+        # self.__class__(x, y, a, b)
+
+        # Case 2: self.x == other.x, self.y != other.y
+        # Result is point at infinity
+        # Remember to return an instance of this class:
+        # self.__class__(x, y, a, b)
+
+        # Case 3: self.x == other.x, self.y == other.y
+        # Formula (x3,y3)=(x1,y1)+(x1,y1)
+        # s=(3*x1**2+a)/(2*y1)
+        # x3=s**2-2x1
+        # y3=s*(x1-x3)-y1
+        # Remember to return an instance of this class:
+        # self.__class__(x, y, a, b)
+        raise NotImplementedError
+
+
+class PointTest(TestCase):
+
+    def test_on_curve(self):
+        with self.assertRaises(RuntimeError):
+            Point(x=-2, y=4, a=5, b=7)
+        # these should not raise an error
+        Point(x=3, y=-7, a=5, b=7)
+        Point(x=18, y=77, a=5, b=7)
+        Point(x=None, y=None, a=5, b=7)
+
+    def test_add0(self):
+        a = Point(x=None, y=None, a=5, b=7)
+        b = Point(x=2, y=5, a=5, b=7)
+        c = Point(x=2, y=-5, a=5, b=7)
+        self.assertEqual(a+b, b)
+        self.assertEqual(b+a, b)
+        self.assertEqual(b+c, a)
+    
+    def test_add1(self):
+        a = Point(x=3, y=7, a=5, b=7)
+        b = Point(x=-1, y=-1, a=5, b=7)
+        self.assertEqual(a+b, Point(x=2, y=-5, a=5, b=7))
+
+    def test_add2(self):
+        a = Point(x=-1, y=1, a=5, b=7)
+        self.assertEqual(a+a, Point(x=18, y=-77, a=5, b=7))
