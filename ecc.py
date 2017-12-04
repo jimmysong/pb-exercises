6a67c597a995156624c924e9d54b6d32ede3c3fc
Jimmy Song <jaejoon@gmail.com>
2017-12-04 13:38:30 -0800

    session1 complete

--- ecc.py
+++ ecc.py
@@ -25,42 +25,60 @@ class FieldElement:
         return 'FieldElement_{}({})'.format(self.prime, self.num)
 
     def __add__(self, other):
+        if self.prime != other.prime:
+            raise RuntimeError('Primes must be the same')
         # self.num and other.num are the actual values
+        num = (self.num + other.num) % self.prime
         # self.prime is what you'll need to mod against
+        prime = self.prime
         # You need to return an element of the same class
         # use: self.__class__(num, prime)
-        raise NotImplementedError
+        return self.__class__(num, prime)
 
     def __sub__(self, other):
+        if self.prime != other.prime:
+            raise RuntimeError('Primes must be the same')
         # self.num and other.num are the actual values
+        num = (self.num - other.num) % self.prime
         # self.prime is what you'll need to mod against
+        prime = self.prime
         # You need to return an element of the same class
         # use: self.__class__(num, prime)
-        raise NotImplementedError
+        return self.__class__(num, prime)
 
     def __mul__(self, other):
+        if self.prime != other.prime:
+            raise RuntimeError('Primes must be the same')
         # self.num and other.num are the actual values
+        num = (self.num * other.num) % self.prime
         # self.prime is what you'll need to mod against
+        prime = self.prime
         # You need to return an element of the same class
         # use: self.__class__(num, prime)
-        raise NotImplementedError
+        return self.__class__(num, prime)
 
     def __pow__(self, n):
         # remember fermat's little theorem:
         # self.num**(p-1) % p == 1
         # you might want to use % operator on n
-        raise NotImplementedError
+        prime = self.prime
+        num = pow(self.num, n % (prime-1), prime)
+        return self.__class__(num, prime)
 
     def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise RuntimeError('Primes must be the same')
         # self.num and other.num are the actual values
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
         # self.prime is what you'll need to mod against
+        prime = self.prime
         # use fermat's little theorem:
         # self.num**(p-1) % p == 1
         # this means:
         # 1/n == pow(n, p-2, p)
         # You need to return an element of the same class
         # use: self.__class__(num, prime)
-        raise NotImplementedError
+        return self.__class__(num, prime)
 
 
 
@@ -116,9 +134,13 @@ class Point:
         # x being None and y being None represents the point at infinity
         # Check for that here since the equation below won't make sense
         # with None values for both.
+        if self.x is None and self.y is None:
+            return
         # make sure that the elliptic curve equation is satisfied
         # y**2 == x**3 + a*x + b
+        if self.y**2 != self.x**3 + a*x + b:
         # if not, throw a RuntimeError
+            raise RuntimeError('({}, {}) is not on the curve'.format(self.x, self.y))
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y \
@@ -135,30 +157,47 @@ class Point:
             return 'Point({},{})'.format(self.x, self.y)
 
     def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise RuntimeError('Points {}, {} are not on the same curve'.format(self, other))
         # Case 0.0: self is the point at infinity, return other
+        if self.x is None:
+            return other
         # Case 0.1: other is the point at infinity, return self
+        if other.x is None:
+            return self
 
-        # Case 1: self.x != other.x
+        # Case 1: self.x == other.x, self.y != other.y
+        # Result is point at infinity
+        if self.x == other.x and self.y != other.y:
+        # Remember to return an instance of this class:
+        # self.__class__(x, y, a, b)
+            return self.__class__(None, None, self.a, self.b)
+ 
+        # Case 2: self.x != other.x
+        if self.x != other.x:
         # Formula (x3,y3)==(x1,y1)+(x2,y2)
         # s=(y2-y1)/(x2-x1)
+            s = (other.y - self.y) / (other.x - self.x)
         # x3=s**2-x1-x2
+            x = s**2 - self.x - other.x
         # y3=s*(x1-x3)-y1
+            y = s*(self.x-x) - self.y
         # Remember to return an instance of this class:
         # self.__class__(x, y, a, b)
-
-        # Case 2: self.x == other.x, self.y != other.y
-        # Result is point at infinity
-        # Remember to return an instance of this class:
-        # self.__class__(x, y, a, b)
+            return self.__class__(x, y, self.a, self.b)
 
         # Case 3: self.x == other.x, self.y == other.y
+        else:
         # Formula (x3,y3)=(x1,y1)+(x1,y1)
         # s=(3*x1**2+a)/(2*y1)
-        # x3=s**2-2x1
+            s = (3*self.x**2 + self.a) / (2*self.y)
+        # x3=s**2-2*x1
+            x = s**2 - 2*self.x
         # y3=s*(x1-x3)-y1
+            y = s*(self.x-x) - self.y
         # Remember to return an instance of this class:
         # self.__class__(x, y, a, b)
-        raise NotImplementedError
+            return self.__class__(x, y, self.a, self.b)
 
 
 class PointTest(TestCase):
