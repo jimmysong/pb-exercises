ecb5a14ac617d5dc63cc5a0d869e939e52cc7888
Jimmy Song <jaejoon@gmail.com>
2018-01-03 15:49:08 -0800

    s6-c

--- block.py
+++ block.py
@@ -5,57 +5,18 @@ from helper import (
     double_sha256,
     int_to_little_endian,
     little_endian_to_int,
-    merkle_parent,
-    merkle_parent_level,
-    merkle_path,
-    merkle_root,
 )
 
 
-class Proof:
-
-    def __init__(self, merkle_root, tx_hash, index, proof_hashes):
-        self.merkle_root = merkle_root
-        self.tx_hash = tx_hash
-        self.index = index
-        self.proof_hashes = proof_hashes
-
-    def __repr__(self):
-        s = '{}:{}:{}:['.format(
-            self.merkle_root.hex(),
-            self.tx_hash.hex(),
-            self.index,
-        )
-        for p in self.proof_hashes:
-            s += '{},'.format(p.hex())
-        s += ']'
-        return s
-
-    def verify(self):
-        '''Returns whether this proof is valid'''
-        # current hash starts with self.tx_hash, reversed
-        # initialize the current_index to be the index at at base level
-        # Loop through proof hashes
-            # if current_index is odd, proof_hash goes on left
-                # current hash becomes merkle parent of proof_hash and current
-            # if current_index is even, proof_hash goes on right
-                # current hash becomes merkle parent of current and proof_hash
-            # update the current_index to be integer divide by 2
-        # if final result reversed is equal to merkle_root, return True
-        raise NotImplementedError
-
-    
 class Block:
 
-    def __init__(self, version, prev_block, merkle_root, timestamp, bits, nonce, tx_hashes=None):
+    def __init__(self, version, prev_block, merkle_root, timestamp, bits, nonce):
         self.version = version
         self.prev_block = prev_block
         self.merkle_root = merkle_root
         self.timestamp = timestamp
         self.bits = bits
         self.nonce = nonce
-        self.tx_hashes = tx_hashes
-        self.merkle_tree = None
 
     @classmethod
     def parse(cls, s):
@@ -146,41 +107,6 @@ class Block:
         # return whether this integer is less than the target
         return proof < self.target()
 
-    def validate_merkle_root(self):
-        '''Gets the merkle root of the tx_hashes and checks that it's
-        the same as the merkle root of this block.
-        '''
-        # reverse all the transaction hashes (self.tx_hashes)
-        # get the Merkle Root
-        # reverse the Merkle Root
-        # return whether self.merkle root is the same as 
-        # the reverse of the calculated merkle root
-        raise NotImplementedError
-
-    def calculate_merkle_tree(self):
-        '''Calculate and store the entire Merkle Tree'''
-        # store the result in self.merkle_tree, an array, 0 representing
-        # the bottom level and 1 the parent level of level 0 and so on.
-        # initialize self.merkle_tree to be an empty list
-        # reverse all the transaction hashes (self.tx_hashes) store as current level
-        # while there is more than 1 hash:
-            # store current level in self.merkle_tree
-            # Make current level Merkle Parent level
-        raise NotImplementedError
-
-    def create_merkle_proof(self, tx_hash):
-        # if self.merkle_tree is empty, go and calculate the merkle tree
-        # find the index of this tx_hash
-        # initialize proof hashes
-        # initialize the current index to be the index at the base level
-        # Loop over the levels in the merkle tree
-            # Find the partner index (-1 for odd, +1 for even)
-            # partner is at the level's partner index
-            # add partner to proof hashes
-            # update the current_index to be integer divide by 2
-        # Return a Proof instance Proof(root, tx_hash, index, proof_hashes)
-        raise NotImplementedError
-
 
 class BlockTest(TestCase):
 
@@ -256,125 +182,3 @@ class BlockTest(TestCase):
         stream = BytesIO(block_raw)
         block = Block.parse(stream)
         self.assertFalse(block.check_pow())
-
-    def test_validate_merkle_root(self):
-        hashes_hex = [
-            'f54cb69e5dc1bd38ee6901e4ec2007a5030e14bdd60afb4d2f3428c88eea17c1',
-            'c57c2d678da0a7ee8cfa058f1cf49bfcb00ae21eda966640e312b464414731c1',
-            'b027077c94668a84a5d0e72ac0020bae3838cb7f9ee3fa4e81d1eecf6eda91f3',
-            '8131a1b8ec3a815b4800b43dff6c6963c75193c4190ec946b93245a9928a233d',
-            'ae7d63ffcb3ae2bc0681eca0df10dda3ca36dedb9dbf49e33c5fbe33262f0910',
-            '61a14b1bbdcdda8a22e61036839e8b110913832efd4b086948a6a64fd5b3377d',
-            'fc7051c8b536ac87344c5497595d5d2ffdaba471c73fae15fe9228547ea71881',
-            '77386a46e26f69b3cd435aa4faac932027f58d0b7252e62fb6c9c2489887f6df',
-            '59cbc055ccd26a2c4c4df2770382c7fea135c56d9e75d3f758ac465f74c025b8',
-            '7c2bf5687f19785a61be9f46e031ba041c7f93e2b7e9212799d84ba052395195',
-            '08598eebd94c18b0d59ac921e9ba99e2b8ab7d9fccde7d44f2bd4d5e2e726d2e',
-            'f0bb99ef46b029dd6f714e4b12a7d796258c48fee57324ebdc0bbc4700753ab1',
-        ]
-        hashes = [bytes.fromhex(x) for x in hashes_hex]
-        stream = BytesIO(bytes.fromhex('00000020fcb19f7895db08cadc9573e7915e3919fb76d59868a51d995201000000000000acbcab8bcc1af95d8d563b77d24c3d19b18f1486383d75a5085c4e86c86beed691cfa85916ca061a00000000'))
-        block = Block.parse(stream)
-        block.tx_hashes = hashes
-        self.assertTrue(block.validate_merkle_root())
-
-    def test_calculate_merkle_tree(self):
-        hashes_hex = [
-            'f54cb69e5dc1bd38ee6901e4ec2007a5030e14bdd60afb4d2f3428c88eea17c1',
-            'c57c2d678da0a7ee8cfa058f1cf49bfcb00ae21eda966640e312b464414731c1',
-            'b027077c94668a84a5d0e72ac0020bae3838cb7f9ee3fa4e81d1eecf6eda91f3',
-            '8131a1b8ec3a815b4800b43dff6c6963c75193c4190ec946b93245a9928a233d',
-            'ae7d63ffcb3ae2bc0681eca0df10dda3ca36dedb9dbf49e33c5fbe33262f0910',
-            '61a14b1bbdcdda8a22e61036839e8b110913832efd4b086948a6a64fd5b3377d',
-            'fc7051c8b536ac87344c5497595d5d2ffdaba471c73fae15fe9228547ea71881',
-            '77386a46e26f69b3cd435aa4faac932027f58d0b7252e62fb6c9c2489887f6df',
-            '59cbc055ccd26a2c4c4df2770382c7fea135c56d9e75d3f758ac465f74c025b8',
-            '7c2bf5687f19785a61be9f46e031ba041c7f93e2b7e9212799d84ba052395195',
-            '08598eebd94c18b0d59ac921e9ba99e2b8ab7d9fccde7d44f2bd4d5e2e726d2e',
-            'f0bb99ef46b029dd6f714e4b12a7d796258c48fee57324ebdc0bbc4700753ab1',
-        ]
-        hashes = [bytes.fromhex(x) for x in hashes_hex]
-        stream = BytesIO(bytes.fromhex('00000020fcb19f7895db08cadc9573e7915e3919fb76d59868a51d995201000000000000acbcab8bcc1af95d8d563b77d24c3d19b18f1486383d75a5085c4e86c86beed691cfa85916ca061a00000000'))
-        block = Block.parse(stream)
-        block.tx_hashes = hashes
-        block.calculate_merkle_tree()
-        want0 = [
-            'c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5',
-            'c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5',
-            'f391da6ecfeed1814efae39e7fcb3838ae0b02c02ae7d0a5848a66947c0727b0',
-            '3d238a92a94532b946c90e19c49351c763696cff3db400485b813aecb8a13181',
-            '10092f2633be5f3ce349bf9ddbde36caa3dd10dfa0ec8106bce23acbff637dae',
-            '7d37b3d54fa6a64869084bfd2e831309118b9e833610e6228adacdbd1b4ba161',
-            '8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc',
-            'dff6879848c2c9b62fe652720b8df5272093acfaa45a43cdb3696fe2466a3877',
-            'b825c0745f46ac58f7d3759e6dc535a1fec7820377f24d4c2c6ad2cc55c0cb59',
-            '95513952a04bd8992721e9b7e2937f1c04ba31e0469fbe615a78197f68f52b7c',
-            '2e6d722e5e4dbdf2447ddecc9f7dabb8e299bae921c99ad5b0184cd9eb8e5908',
-            'b13a750047bc0bdceb2473e5fe488c2596d7a7124b4e716fdd29b046ef99bbf0',
-        ]
-        want1 = [
-            '8b30c5ba100f6f2e5ad1e2a742e5020491240f8eb514fe97c713c31718ad7ecd',
-            '7f4e6f9e224e20fda0ae4c44114237f97cd35aca38d83081c9bfd41feb907800',
-            'ade48f2bbb57318cc79f3a8678febaa827599c509dce5940602e54c7733332e7',
-            '68b3e2ab8182dfd646f13fdf01c335cf32476482d963f5cd94e934e6b3401069',
-            '43e7274e77fbe8e5a42a8fb58f7decdb04d521f319f332d88e6b06f8e6c09e27',
-            '4f492e893bf854111c36cb5eff4dccbdd51b576e1cfdc1b84b456cd1c0403ccb',
-        ]
-        want2 = [
-            '26906cb2caeb03626102f7606ea332784281d5d20e2b4839fbb3dbb37262dbc1',
-            '717a0d17538ff5ad2c020bab38bdcde66e63f3daef88f89095f344918d5d4f96',
-            'd20629030c7e48e778c1c837d91ebadc2f2ee319a0a0a461f4a9538b5cae2a69',
-            'd20629030c7e48e778c1c837d91ebadc2f2ee319a0a0a461f4a9538b5cae2a69',
-        ]
-        want3 = [
-            'b9f5560ce9630ea4177a7ac56d18dea73c8f76b59e02ab4805eaeebd84a4c5b1',
-            '00aa9ad6a7841ffbbf262eb775f8357674f1ea23af11c01cfb6d481fec879701',
-        ]
-        self.assertEqual(block.merkle_tree[0], [bytes.fromhex(x) for x in want0])
-        self.assertEqual(block.merkle_tree[1], [bytes.fromhex(x) for x in want1])
-        self.assertEqual(block.merkle_tree[2], [bytes.fromhex(x) for x in want2])
-        self.assertEqual(block.merkle_tree[3], [bytes.fromhex(x) for x in want3])
-
-    def test_create_merkle_proof(self):
-        hashes_hex = [
-            'f54cb69e5dc1bd38ee6901e4ec2007a5030e14bdd60afb4d2f3428c88eea17c1',
-            'c57c2d678da0a7ee8cfa058f1cf49bfcb00ae21eda966640e312b464414731c1',
-            'b027077c94668a84a5d0e72ac0020bae3838cb7f9ee3fa4e81d1eecf6eda91f3',
-            '8131a1b8ec3a815b4800b43dff6c6963c75193c4190ec946b93245a9928a233d',
-            'ae7d63ffcb3ae2bc0681eca0df10dda3ca36dedb9dbf49e33c5fbe33262f0910',
-            '61a14b1bbdcdda8a22e61036839e8b110913832efd4b086948a6a64fd5b3377d',
-            'fc7051c8b536ac87344c5497595d5d2ffdaba471c73fae15fe9228547ea71881',
-            '77386a46e26f69b3cd435aa4faac932027f58d0b7252e62fb6c9c2489887f6df',
-            '59cbc055ccd26a2c4c4df2770382c7fea135c56d9e75d3f758ac465f74c025b8',
-            '7c2bf5687f19785a61be9f46e031ba041c7f93e2b7e9212799d84ba052395195',
-            '08598eebd94c18b0d59ac921e9ba99e2b8ab7d9fccde7d44f2bd4d5e2e726d2e',
-            'f0bb99ef46b029dd6f714e4b12a7d796258c48fee57324ebdc0bbc4700753ab1',
-        ]
-        hashes = [bytes.fromhex(x) for x in hashes_hex]
-        stream = BytesIO(bytes.fromhex('00000020fcb19f7895db08cadc9573e7915e3919fb76d59868a51d995201000000000000acbcab8bcc1af95d8d563b77d24c3d19b18f1486383d75a5085c4e86c86beed691cfa85916ca061a00000000'))
-        block = Block.parse(stream)
-        block.tx_hashes = hashes
-        h = hashes[7]
-        proof = block.create_merkle_proof(h)
-        self.assertEqual(proof.index, 7)
-        want = [
-            '8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc',
-            'ade48f2bbb57318cc79f3a8678febaa827599c509dce5940602e54c7733332e7',
-            '26906cb2caeb03626102f7606ea332784281d5d20e2b4839fbb3dbb37262dbc1',
-            '00aa9ad6a7841ffbbf262eb775f8357674f1ea23af11c01cfb6d481fec879701',
-        ]
-        self.assertEqual(proof.proof_hashes, [bytes.fromhex(x) for x in want])
-
-    def test_verify_merkle_proof(self):
-        merkle_root = bytes.fromhex('d6ee6bc8864e5c08a5753d3886148fb1193d4cd2773b568d5df91acc8babbcac')
-        tx_hash = bytes.fromhex('77386a46e26f69b3cd435aa4faac932027f58d0b7252e62fb6c9c2489887f6df')
-        index = 7
-        proof_hex_hashes = [
-            '8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc',
-            'ade48f2bbb57318cc79f3a8678febaa827599c509dce5940602e54c7733332e7',
-            '26906cb2caeb03626102f7606ea332784281d5d20e2b4839fbb3dbb37262dbc1',
-            '00aa9ad6a7841ffbbf262eb775f8357674f1ea23af11c01cfb6d481fec879701',
-        ]
-        proof_hashes = [bytes.fromhex(x) for x in proof_hex_hashes]
-        proof = Proof(merkle_root=merkle_root, tx_hash=tx_hash, index=index, proof_hashes=proof_hashes)
-        self.assertTrue(proof.verify())
--- helper.py
+++ helper.py
@@ -2,7 +2,6 @@ from subprocess import check_output
 from unittest import TestCase, TestSuite, TextTestRunner
 
 import hashlib
-import math
 
 
 SIGHASH_ALL = 1
@@ -140,47 +139,6 @@ def h160_to_p2sh_address(h160, testnet=False):
     return encode_base58_checksum(prefix + h160)
 
 
-def merkle_parent(hash1, hash2):
-    '''Takes the binary hashes and calculates the double-sha256'''
-    # return the double-sha256 of hash1 + hash2
-    raise NotImplementedError
-
-def merkle_parent_level(hash_list):
-    '''Takes a list of binary hashes and returns a list that's half
-    the length'''
-    # Exercise 2.2: if the list has exactly 1 element raise an error
-    # Exercise 3.2: if the list has an odd number of elements, duplicate the last one
-    #               and put it at the end so it has an even number of elements
-    # Exercise 2.2: initialize next level
-    # Exercise 2.2: loop over every pair (use: for i in range(0, len(hash_list), 2))
-        # Exercise 2.2: get the merkle parent of i and i+1 hashes
-        # Exercise 2.2: append parent to parent level
-    # Exercise 2.2: return parent level
-    raise NotImplementedError
-
-
-def merkle_root(hash_list):
-    '''Takes a list of binary hashes and returns the merkle root
-    '''
-    # current level starts as hash_list
-    # loop until there's exactly 1 element
-        # current level becomes the merkle parent level
-    # return the 1st item of current_level
-    raise NotImplementedError
-
-
-def merkle_path(index, total):
-    '''Returns a list of indexes up the merkle tree of the node at index if
-    there are a total number of nodes'''
-    # initialize the path
-    # calculate number of levels (math.ceil(math.log(total, 2)))
-    # loop through each level
-        # add the index to path
-        # index becomes integer divide by 2 (use: index = index // 2)
-    # return the path
-    raise NotImplementedError
-
-
 class HelperTest(TestCase):
 
     def test_bytes(self):
@@ -226,89 +184,3 @@ class HelperTest(TestCase):
         self.assertEqual(h160_to_p2sh_address(h160, testnet=False), want)
         want = '2N3u1R6uwQfuobCqbCgBkpsgBxvr1tZpe7B'
         self.assertEqual(h160_to_p2sh_address(h160, testnet=True), want)
-
-    def test_merkle_parent(self):
-        tx_hash0 = bytes.fromhex('c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5')
-        tx_hash1 = bytes.fromhex('c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5')
-        want = bytes.fromhex('8b30c5ba100f6f2e5ad1e2a742e5020491240f8eb514fe97c713c31718ad7ecd')
-        self.assertEqual(merkle_parent(tx_hash0, tx_hash1), want)
-
-    def test_merkle_parent_level0(self):
-        hex_hashes = [
-            'c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5',
-            'c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5',
-            'f391da6ecfeed1814efae39e7fcb3838ae0b02c02ae7d0a5848a66947c0727b0',
-            '3d238a92a94532b946c90e19c49351c763696cff3db400485b813aecb8a13181',
-            '10092f2633be5f3ce349bf9ddbde36caa3dd10dfa0ec8106bce23acbff637dae',
-            '7d37b3d54fa6a64869084bfd2e831309118b9e833610e6228adacdbd1b4ba161',
-            '8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc',
-            'dff6879848c2c9b62fe652720b8df5272093acfaa45a43cdb3696fe2466a3877',
-            'b825c0745f46ac58f7d3759e6dc535a1fec7820377f24d4c2c6ad2cc55c0cb59',
-            '95513952a04bd8992721e9b7e2937f1c04ba31e0469fbe615a78197f68f52b7c',
-            '2e6d722e5e4dbdf2447ddecc9f7dabb8e299bae921c99ad5b0184cd9eb8e5908',
-            'b13a750047bc0bdceb2473e5fe488c2596d7a7124b4e716fdd29b046ef99bbf0',
-        ]
-        tx_hashes = [bytes.fromhex(x) for x in hex_hashes]
-        want_hex_hashes = [
-            '8b30c5ba100f6f2e5ad1e2a742e5020491240f8eb514fe97c713c31718ad7ecd',
-            '7f4e6f9e224e20fda0ae4c44114237f97cd35aca38d83081c9bfd41feb907800',
-            'ade48f2bbb57318cc79f3a8678febaa827599c509dce5940602e54c7733332e7',
-            '68b3e2ab8182dfd646f13fdf01c335cf32476482d963f5cd94e934e6b3401069',
-            '43e7274e77fbe8e5a42a8fb58f7decdb04d521f319f332d88e6b06f8e6c09e27',
-            '4f492e893bf854111c36cb5eff4dccbdd51b576e1cfdc1b84b456cd1c0403ccb',
-        ]
-        want_tx_hashes = [bytes.fromhex(x) for x in want_hex_hashes]
-        self.assertEqual(merkle_parent_level(tx_hashes), want_tx_hashes)
-
-    def test_merkle_parent_level1(self):
-        hex_hashes = [
-            'c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5',
-            'c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5',
-            'f391da6ecfeed1814efae39e7fcb3838ae0b02c02ae7d0a5848a66947c0727b0',
-            '3d238a92a94532b946c90e19c49351c763696cff3db400485b813aecb8a13181',
-            '10092f2633be5f3ce349bf9ddbde36caa3dd10dfa0ec8106bce23acbff637dae',
-            '7d37b3d54fa6a64869084bfd2e831309118b9e833610e6228adacdbd1b4ba161',
-            '8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc',
-            'dff6879848c2c9b62fe652720b8df5272093acfaa45a43cdb3696fe2466a3877',
-            'b825c0745f46ac58f7d3759e6dc535a1fec7820377f24d4c2c6ad2cc55c0cb59',
-            '95513952a04bd8992721e9b7e2937f1c04ba31e0469fbe615a78197f68f52b7c',
-            '2e6d722e5e4dbdf2447ddecc9f7dabb8e299bae921c99ad5b0184cd9eb8e5908',
-        ]
-        tx_hashes = [bytes.fromhex(x) for x in hex_hashes]
-        want_hex_hashes = [
-            '8b30c5ba100f6f2e5ad1e2a742e5020491240f8eb514fe97c713c31718ad7ecd',
-            '7f4e6f9e224e20fda0ae4c44114237f97cd35aca38d83081c9bfd41feb907800',
-            'ade48f2bbb57318cc79f3a8678febaa827599c509dce5940602e54c7733332e7',
-            '68b3e2ab8182dfd646f13fdf01c335cf32476482d963f5cd94e934e6b3401069',
-            '43e7274e77fbe8e5a42a8fb58f7decdb04d521f319f332d88e6b06f8e6c09e27',
-            '1796cd3ca4fef00236e07b723d3ed88e1ac433acaaa21da64c4b33c946cf3d10',
-        ]
-        want_tx_hashes = [bytes.fromhex(x) for x in want_hex_hashes]
-        self.assertEqual(merkle_parent_level(tx_hashes), want_tx_hashes)
-
-
-    def test_merkle_root(self):
-        hex_hashes = [
-            'c117ea8ec828342f4dfb0ad6bd140e03a50720ece40169ee38bdc15d9eb64cf5',
-            'c131474164b412e3406696da1ee20ab0fc9bf41c8f05fa8ceea7a08d672d7cc5',
-            'f391da6ecfeed1814efae39e7fcb3838ae0b02c02ae7d0a5848a66947c0727b0',
-            '3d238a92a94532b946c90e19c49351c763696cff3db400485b813aecb8a13181',
-            '10092f2633be5f3ce349bf9ddbde36caa3dd10dfa0ec8106bce23acbff637dae',
-            '7d37b3d54fa6a64869084bfd2e831309118b9e833610e6228adacdbd1b4ba161',
-            '8118a77e542892fe15ae3fc771a4abfd2f5d5d5997544c3487ac36b5c85170fc',
-            'dff6879848c2c9b62fe652720b8df5272093acfaa45a43cdb3696fe2466a3877',
-            'b825c0745f46ac58f7d3759e6dc535a1fec7820377f24d4c2c6ad2cc55c0cb59',
-            '95513952a04bd8992721e9b7e2937f1c04ba31e0469fbe615a78197f68f52b7c',
-            '2e6d722e5e4dbdf2447ddecc9f7dabb8e299bae921c99ad5b0184cd9eb8e5908',
-            'b13a750047bc0bdceb2473e5fe488c2596d7a7124b4e716fdd29b046ef99bbf0',
-        ]
-        tx_hashes = [bytes.fromhex(x) for x in hex_hashes]
-        want_hex_hash = 'acbcab8bcc1af95d8d563b77d24c3d19b18f1486383d75a5085c4e86c86beed6'
-        want_hash = bytes.fromhex(want_hex_hash)
-        self.assertEqual(merkle_root(tx_hashes), want_hash)
-
-    def test_merkle_path(self):
-        i = 7
-        total = 11
-        want = [7, 3, 1, 0]
-        self.assertEqual(merkle_path(i, total), want)
