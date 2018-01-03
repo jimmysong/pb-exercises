5a663291be02e40375cb677fba27c37a63d1f464
Jimmy Song <jaejoon@gmail.com>
2018-01-03 15:34:09 -0800

    s7

--- network.py
+++ network.py
@@ -27,36 +27,22 @@ class NetworkEnvelope:
     def parse(cls, s):
         '''Takes a stream and creates a NetworkEnvelope'''
         # check the network magic NETWORK_MAGIC
-        magic = s.read(4)
-        if magic != NETWORK_MAGIC:
-            raise RuntimeError('magic is not right')
+        # check the network magic b'\xf9\xbe\xb4\xd9'
         # command 12 bytes
-        command = s.read(12)
         # payload length 4 bytes, little endian
-        payload_length = little_endian_to_int(s.read(4))
         # checksum 4 bytes, first four of double-sha256 of payload
-        checksum = s.read(4)
         # payload is of length payload_length
-        payload = s.read(payload_length)
         # verify checksum
-        calculated_checksum = double_sha256(payload)[:4]
-        if calculated_checksum != checksum:
-            raise RuntimeError('checksum does not match')
-        return cls(command, payload)
+        raise NotImplementedError
 
     def serialize(self):
         '''Returns the byte serialization of the entire network message'''
         # add the network magic NETWORK_MAGIC
-        result = NETWORK_MAGIC
         # command 12 bytes
-        result += self.command
         # payload length 4 bytes, little endian
-        result += int_to_little_endian(len(self.payload), 4)
         # checksum 4 bytes, first four of double-sha256 of payload
-        result += double_sha256(self.payload)[:4]
         # payload
-        result += self.payload
-        return result
+        raise NotImplementedError
 
 
 class NetworkEnvelopeTest(TestCase):
