0e07842cf67c6021b67d33c7e563318de74a6c31
Jimmy Song <jaejoon@gmail.com>
2018-01-03 17:28:09 -0800

    s2-c

--- helper.py
+++ helper.py
@@ -63,52 +63,6 @@ def encode_base58(s):
     return prefix + bytes(result)
 
 
-def encode_base58_checksum(s):
-    return encode_base58(s + double_sha256(s)[:4]).decode('ascii')
-
-
-def decode_base58(s):
-    num = 0
-    for c in s.encode('ascii'):
-        num *= 58
-        num += BASE58_ALPHABET.index(c)
-    combined = num.to_bytes(25, byteorder='big')
-    checksum = combined[-4:]
-    if double_sha256(combined[:-4])[:4] != checksum:
-        raise RuntimeError('bad address: {} {}'.format(checksum, double_sha256(combined)[:4]))
-    return combined[1:-4]
-
-
-def read_varint(s):
-    '''read_varint reads a variable integer from a stream'''
-    i = s.read(1)[0]
-    if i == 0xfd:
-        # 0xfd means the next two bytes are the number
-        return little_endian_to_int(s.read(2))
-    elif i == 0xfe:
-        # 0xfe means the next four bytes are the number
-        return little_endian_to_int(s.read(4))
-    elif i == 0xff:
-        # 0xff means the next eight bytes are the number
-        return little_endian_to_int(s.read(8))
-    else:
-        # anything else is just the integer
-        return i
-
-
-def encode_varint(i):
-    '''encodes an integer as a varint'''
-    if i < 0xfd:
-        return bytes([i])
-    elif i < 0x10000:
-        return b'\xfd' + int_to_little_endian(i, 2)
-    elif i < 0x100000000:
-        return b'\xfe' + int_to_little_endian(i, 4)
-    elif i < 0x10000000000000000:
-        return b'\xff' + int_to_little_endian(i, 8)
-    else:
-        raise RuntimeError('integer too large: {}'.format(i))
-
 
 class HelperTest(TestCase):
 
