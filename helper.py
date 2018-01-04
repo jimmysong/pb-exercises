3b5293faf2b95237372cce81fa570b50274f23fd
Jimmy Song <jaejoon@gmail.com>
2018-01-03 17:49:14 -0800

    s0

--- helper.py
+++ helper.py
@@ -9,26 +9,26 @@ def run_test(test):
 
 def bytes_to_str(b, encoding='ascii'):
     '''Returns a string version of the bytes'''
-    return b.decode(encoding)
+    raise NotImplementedError
 
 
 def str_to_bytes(s, encoding='ascii'):
     '''Returns a bytes version of the string'''
-    return s.encode(encoding)
+    raise NotImplementedError
 
 
 def little_endian_to_int(b):
     '''little_endian_to_int takes byte sequence as a little-endian number.
     Returns an integer'''
     # use the from_bytes method of int
-    return int.from_bytes(b, 'little')
+    raise NotImplementedError
 
 
 def int_to_little_endian(n, length):
     '''endian_to_little_endian takes an integer and returns the little-endian
     byte sequence of length'''
     # use the to_bytes method of n
-    return n.to_bytes(length, 'little')
+    raise NotImplementedError
 
 
 class HelperTest(TestCase):
