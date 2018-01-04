26581d9b028a80e9d495aac0d97bf415e1295790
Jimmy Song <jaejoon@gmail.com>
2018-01-03 16:13:51 -0800

    s5

--- helper.py
+++ helper.py
@@ -122,21 +122,13 @@ def encode_varint(i):
 def h160_to_p2pkh_address(h160, testnet=False):
     '''Takes a byte sequence hash160 and returns a p2pkh address string'''
     # p2pkh has a prefix of b'\x00' for mainnet, b'\x6f' for testnet
-    if testnet:
-        prefix = b'\x6f'
-    else:
-        prefix = b'\x00'
-    return encode_base58_checksum(prefix + h160)
+    raise NotImplementedError
 
 
 def h160_to_p2sh_address(h160, testnet=False):
     '''Takes a byte sequence hash160 and returns a p2sh address string'''
-    # p2sh has a prefix of b'\x05' for mainnet, b'\xc4' for testnet
-    if testnet:
-        prefix = b'\xc4'
-    else:
-        prefix = b'\x05'
-    return encode_base58_checksum(prefix + h160)
+    # p2sh has a prefix of b'\x05' for mainnet, b'\xc4 for testnet
+    raise NotImplementedError
 
 
 class HelperTest(TestCase):
