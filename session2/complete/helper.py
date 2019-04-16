b15a8650512cd2828eb212d5a483de5f6da7c0c1
Jimmy Song <jaejoon@gmail.com>
2019-04-15 19:40:48 -0500

    s0 jupyter

--- session2/complete/helper.py
+++ session2/complete/helper.py
@@ -14,25 +14,27 @@ def run(test):
 
 def bytes_to_str(b, encoding='ascii'):
     '''Returns a string version of the bytes'''
+    # use the bytes.decode(encoding) method
     return b.decode(encoding)
 
 
 def str_to_bytes(s, encoding='ascii'):
     '''Returns a bytes version of the string'''
+    # use the string.encode(encoding) method
     return s.encode(encoding)
 
 
 def little_endian_to_int(b):
     '''little_endian_to_int takes byte sequence as a little-endian number.
     Returns an integer'''
-    # use the from_bytes method of int
+    # use the int.from_bytes(b, <endianness>) method
     return int.from_bytes(b, 'little')
 
 
 def int_to_little_endian(n, length):
     '''endian_to_little_endian takes an integer and returns the little-endian
     byte sequence of length'''
-    # use the to_bytes method of n
+    # use the int.to_bytes(length, <endianness>) method
     return n.to_bytes(length, 'little')
 
 
