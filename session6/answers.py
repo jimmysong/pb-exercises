7097d5c3064d465f3076c20fab136e844b8b270b
Jimmy Song <jaejoon@gmail.com>
2019-04-15 20:29:32 -0500

    s6 jupyter

--- session6/answers.py
+++ session6/answers.py
@@ -174,6 +174,7 @@ Validate the proof-of-work for this block
 
 Check that the proof-of-work (hash256 interpreted as a little-endian number) is lower than the target.
 ---
+>>> from io import BytesIO
 >>> from block import Block
 >>> hex_block = '04000000fbedbbf0cfdaf278c094f187f2eb987c86a199da22bbb20400000000000000007b7697b29129648fa08b4bcd13c9d5e60abb973a1efac9c8d573c71c807c56c3d6213557faa80518c3737ec1'
 >>> # bytes.fromhex to get the binary block
